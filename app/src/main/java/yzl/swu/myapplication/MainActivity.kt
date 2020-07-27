fd196f921f805e4a1ae982c2c65bac7a04df1b8d
InuyashaY <1426573500@qq.com>
2020-07-27 15:44:10 +0800

    Rotation

--- app/src/main/java/yzl/swu/myapplication/MainActivity.kt
+++ app/src/main/java/yzl/swu/myapplication/MainActivity.kt
@@ -25,40 +25,84 @@ class MainActivity : AppCompatActivity(){
 //        mLoginBtn.setOnClickListener({
 //            println("dsoajsdo")
 //        })
+
+        //apply 添加匿名扩展方法   <T> T.apply(block: T.() -> Unit): T
         mLoginBtn.setOnClickListener {
-            //图片旋转动画    动画结束后跳转界面
-            val anim = ObjectAnimator.ofFloat(imageView2,"rotation",0f,360f)
-            anim.duration = 300
-            anim.repeatCount = 2
-            anim.start()
-
-            //监听动画事件结束
-            anim.addListener(object : Animator.AnimatorListener{
-                override fun onAnimationRepeat(animation: Animator?) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onAnimationEnd(animation: Animator?) {
-                    //界面跳转
-                    startActivity(Intent(this@MainActivity,Logined::class.java))
-                }
-
-                override fun onAnimationCancel(animation: Animator?) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onAnimationStart(animation: Animator?) {
-                    TODO("Not yet implemented")
-                }
-
-            })
-
-            //界面跳转
-            //startActivity(Intent(this,Logined::class.java))
+            ObjectAnimator.ofFloat(imageView2,"rotation",0f,360f).apply {
+                duration = 300
+                repeatCount = 1
+                start()
+                addListener(object :MyAnimatorListener(){
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        //界面跳转
+                        startActivity(Intent(this@MainActivity,Logined::class.java))
+                    }
+                })
+            }
         }
-    }
+
+        //also代码块，it代指使用 also的对象
+        //<T> T.also(block: (T) -> Unit): T
+//        mLoginBtn.setOnClickListener {
+//            ObjectAnimator.ofFloat(imageView2,"rotation",0f,360f).also {
+//                it.duration = 300
+//                it.repeatCount = 1
+//                it.start()
+//                it.addListener(object :Animator.AnimatorListener{
+//                    override fun onAnimationRepeat(animation: Animator?) {
+//
+//                    }
+//
+//                    override fun onAnimationEnd(animation: Animator?) {
+//                        //界面跳转
+//                    startActivity(Intent(this@MainActivity,Logined::class.java))
+//                    }
+//
+//                    override fun onAnimationCancel(animation: Animator?) {
+//
+//                    }
+//
+//                    override fun onAnimationStart(animation: Animator?) {
+//
+//                    }
+//
+//                })
+//            }
+//        }
+//            //图片旋转动画    动画结束后跳转界面
+//            val anim = ObjectAnimator.ofFloat(imageView2,"rotation",0f,360f)
+//            anim.duration = 300
+//            anim.repeatCount = 2
+//            anim.start()
+//
+//            //监听动画事件结束
+//            anim.addListener(object : Animator.AnimatorListener{
+//                override fun onAnimationRepeat(animation: Animator?) {
+//
+//                }
+//
+//                override fun onAnimationEnd(animation: Animator?) {
+//                    //界面跳转
+//                    startActivity(Intent(this@MainActivity,Logined::class.java))
+//                }
+//
+//                override fun onAnimationCancel(animation: Animator?) {
+//
+//                }
+//
+//                override fun onAnimationStart(animation: Animator?) {
+//
+//                }
+//
+//            })
+//
+//            //界面跳转
+//            //startActivity(Intent(this,Logined::class.java))
+//        }
+//    }
 
 //    override fun onClick(v: View?) {
 //        println("here11111")
-//    }
+    }
 }
---
+++ app/src/main/java/yzl/swu/myapplication/MyAnimatorListener.kt
@@ -0,0 +1,20 @@
+package yzl.swu.myapplication
+
+import android.animation.Animator
+
+open class MyAnimatorListener: Animator.AnimatorListener {
+    override fun onAnimationRepeat(animation: Animator?) {
+
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+
+    }
+
+    override fun onAnimationStart(animation: Animator?) {
+
+    }
+}
